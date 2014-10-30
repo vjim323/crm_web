0be6c6caf553c751537d5b04933d6640791635b0
Vincent Jim <vjim323@gmail.com>
2014-10-30 14:20:44 -0400

    allows the search of a specific contact and displays their information

--- crm.rb
+++ crm.rb
@@ -2,7 +2,8 @@ require_relative 'rolodex'
 require_relative 'contact'
 require 'sinatra'
 
-$rolodex = Rolodex.new
+@@rolodex = Rolodex.new
+@@rolodex.add_contact(Contacts.new("Vincent", "Jim", "vjim323@gmail.com", "nothing"))
 
 get '/' do
 	erb :index
@@ -20,4 +21,9 @@ post '/contacts' do
 	new_contact = Contacts.new(params[:first_name], params[:last_name], params[:email], params[:notes])
 	$rolodex.add_contact(new_contact)
 	redirect to('/contacts')
+end
+
+get '/contacts/1000' do
+	@contact = @@rolodex.find(1000)
+	erb :show_contact
 end
