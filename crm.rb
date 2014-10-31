0c4e2fffd743dd8a240f680b81b1033a5080450a
Vincent Jim <vjim323@gmail.com>
2014-10-31 16:57:17 -0400

    removed references to rolodex

--- crm.rb
+++ crm.rb
@@ -1,5 +1,3 @@
-require_relative 'rolodex'
-
 require 'sinatra'
 require 'data_mapper'
 
@@ -17,8 +15,6 @@ end
 
 DataMapper.finalize
 DataMapper.auto_upgrade!
-
-@@rolodex = Rolodex.new
 
 get '/' do
 	erb :index
